be32942dcf6fd3842af41eed58bc80223d4e1717
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-08-11 03:37:17 +0100

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,35 +9,18 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *p2;
-	listint_t *prev;
-
-	p2 = list;
-	prev = list;
-	while (list && p2 && p2->next)
-	{
-		list = list->next;
-		p2 = p2->next->next;
-
-		if (list == p2)
-		{
-			list = prev;
-			prev =  p2;
-			while (1)
-			{
-				p2 = prev;
-				while (p2->next != list && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == list)
-					break;
-
-				list = list->next;
-			}
-			return (1);
-		}
-	}
-
-	return (0);
+	listint_t *slow, *fast;
+    	slow = fast = head;
+   
+    while(slow && fast && fast->next) {
+        /* Slow pointer will move one node per iteration whereas 
+        fast node will move two nodes per iteration */
+        slow = slow->next;
+        fast  = fast->next->next;
+        if (slow == fast) {
+           return (1);
+        }
+    }
+    return (0);
+}
 }
