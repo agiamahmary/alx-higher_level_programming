526aee22d8ca3a27f713fb600447e8091a80a3ba
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-08-29 00:23:32 +0100

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value): 
     try: 
-        print("{:d}\n".format(value)) # Print as an integer 
+        print("{:d}".format(value))
+	print() # Print as an integer 
         return True
     except: 
 	return False
