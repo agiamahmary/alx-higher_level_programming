248e935d27580c7214e2afaa2eeabd5e53f8b0c0
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-08-29 08:41:06 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
+/#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     num = 0
     for item in range(x):
         try:
             print("{}".format(my_list[item]), end='')
             num += 1
-        except IndexError:
+        except Exception:
             break
     print('\n')
     return num
