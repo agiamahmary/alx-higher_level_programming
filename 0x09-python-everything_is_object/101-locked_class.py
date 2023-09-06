d04a1eabee9636bf4ba025ac7564c99fbf9a4ab5
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-09-06 04:15:27 +0100

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""LockedClass module."""
+
 class LockedClass:
+        """LockedClass class containing  only __slots__. attributes as first_name only"""
     __slots__ = ['first_name']
