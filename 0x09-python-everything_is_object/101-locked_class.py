ee534788ce2c0766b920fcbe26f0838885afb1c1
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-10-31 10:16:19 +0100

    Delete 0x09-python-everything_is_object directory

--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""LockedClass module."""
-
-class LockedClass:
-        """LockedClass class containing  only __slots__. attributes as first_name only"""
-    __slots__ = ['first_name']
