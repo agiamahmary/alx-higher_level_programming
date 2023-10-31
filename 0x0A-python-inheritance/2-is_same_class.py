cdfea340f83bea608e63c2dabf69e108496ca533
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-10-31 10:16:46 +0100

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/2-is_same_class.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-This module contains the function is_same_class that test if obj is an object of type a_class
-"""
-
-
-def is_same_class(obj, a_class):
-    """return true if obj is the exact class as a_class, otherwise false"""
-    return (type(obj) == a_class)
