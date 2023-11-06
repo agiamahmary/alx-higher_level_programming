26e0a68b0887c7c98342efc4d8079b67885b8db2
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-06 23:51:54 +0100

    Create 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+""" A module that contains a lookup func
+
+- (for searching for and return the list of available attitributes and methods attached to the object in question)
+
+"""
+def lookup(obj):
+  """ Used to search and return available attributes and method attached to obj """
+  return dir(obj)
