fbfec081f09495732e5a38b1572cdd5747c9e936
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-07 21:21:09 +0100

    Create 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+ #!/usr/bin/python3
+''' Contains a BaseGeometry class 
+
+'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+     """ Inherits from Rectangle """
+     def __init__(self, size):
+         if not self.integer_validator('size', size):
+             self.__size = size
+    def area(self):
+        ''' Area '''
+         return self.__size ** 2
+     def __str__(self):
+         ''' Area '''
+         return "[Rectangle] {}/{}".format(self.__size, self.__size) 
