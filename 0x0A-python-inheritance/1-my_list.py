ade133fb16dd2571cc39612c9946e9e08ec927b2
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-07 18:39:48 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python3
+""" Contains class Mylist """
+
+
 class Mylist(list):
-    """ A class that mimics most of the opearations form superclass list """
-     def print_sorted(self):
+    """ A class that mimics most 
+        of the opearations form superclass list
+    """
+    def print_sorted(self):
          """ Print mylist in ascending order """
          return self.sort()
