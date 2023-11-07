59fbbee7c83a49f4f468aad6b2143a46b35bd621
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-07 03:46:30 +0100

    Create 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,5 @@
+class Mylist(list):
+    """ A class that mimics most of the opearations form superclass list """
+     def print_sorted(self):
+         """ Print mylist in ascending order """
+         return self.sort()
