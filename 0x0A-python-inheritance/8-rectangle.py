e39dc4224fced89ab148333808d74017a9d83af2
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-07 19:39:26 +0100

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ Contains Rectangle class
-that Inherits from BaseGeometry
+    that Inherits from BaseGeometry
 """
+
 
 class BaseGeometry:
     """ METHOD  """
     def area(self):
         """  Raise exception that area is not implemented """
         raise Exception("area() is not impelemented")
-
 
      def integer_validator(self, name, value):
          """ Validates if value is an integer else raise an exception """
