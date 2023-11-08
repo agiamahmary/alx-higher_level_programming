6422e2e1f77492f550d0c31a318e6fb6ce10adf7
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-08 05:43:09 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,6 @@
 
 
 def write_file(filename="", text=''):
-    ''' Writes to a file by appendind to it'''
-    with open(filename, 'a', encoding='utf-8') as f:
+    ''' Writes to a file by overwriting to it'''
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
