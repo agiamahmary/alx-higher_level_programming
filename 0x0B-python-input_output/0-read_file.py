ea680283d9b25d38d0cd3b087a28bfa827ccd356
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-08 05:28:50 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""A modlue that contains read_file func. """
+"""A modlue that contains read_file func."""
 
 
 def read_file(filename=""):
-    """ A func that prints content of a txt file """
+    """A func that prints content of a txt file"""
     with open(filename, 'r', encoding='utf-8') as f:
         print(r.read())
