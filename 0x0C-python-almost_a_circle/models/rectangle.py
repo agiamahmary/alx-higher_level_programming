9e73b5218c9b027e741678d7974d93743dedee4a
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-14 02:54:51 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,7 @@ class Rectangle(Base):
     ''' Rectangle class '''
     def __init__(self, width, height, x=0, y=0, id=None):
         """Init Method"""
-        super().__init__(self, id)
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
