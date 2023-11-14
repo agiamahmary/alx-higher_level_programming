af45447b0dd617be298c21105d1796fbc9ef7cf4
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-14 04:58:34 +0100

    Create square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module contains square class"""
+from .rectangle import Rectangle
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Init method """
+        super().__init__(self, size, x=0, y=0, id)
+        
+    def __str__():
+        """ str method """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
