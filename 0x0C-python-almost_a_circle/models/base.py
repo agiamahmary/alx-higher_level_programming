ddf8419600b41f12550009c4b23259e3cdb546ad
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-14 02:04:53 +0100

    Create base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"Module with Base class"
+
+
+class Base:
+    "Base Class"
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if (id != None):
+            self.id = id
+        else:
+             __nb_objects += 1
+            self.id = id
