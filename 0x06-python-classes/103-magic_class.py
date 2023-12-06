ccb4d04b7b4ea378097395cb5ee0989e3029d59c
maryagiamh <maryagiamh@gmail.com>
2023-12-06 08:34:44 -0800

    yu

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -18,4 +18,4 @@ class MagicClass:
 
     def circumference(self):
         """Calculates the circumference of the circle"""
-        return 2 * math.pi * self.__radiu
+        return 2 * math.pi * self.__radius
