832d07d9bcea711eea406318d8ca2592a7bf145e
maryagiamh <maryagiamh@gmail.com>
2023-12-09 03:46:25 -0800

    They wanted default empty parameter instead

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(def_set=["Best School"]):
-    def_set.append(", " + def_set[0][:])
-    return "".join(def_set[:-1])
+def magic_string(def_set=[]):
+    def_set.append("Best School")
+    return ", ".join(def_set)
