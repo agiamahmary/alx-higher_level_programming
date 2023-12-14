38fbb651d1edf3ab8ae16eaa57d8203e6225cf46
Under20techie <under20techie@gmail.com>
2023-12-14 04:05:47 +0100

    you

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 
-SELECT (score, name) from second_table WHERE NAME IS NOT NULL or name!='' ORDER BY score DESC;
+SELECT ALL (score, name) FROM second_table WHERE NAME IS NOT NULL or name!='' ORDER BY score DESC;
