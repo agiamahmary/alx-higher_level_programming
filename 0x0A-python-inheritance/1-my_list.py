cbda6c5aa57f9df7e6882da854d4edab12b3f27b
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-01 18:49:11 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,9 +3,10 @@
 
 
 class Mylist(list):
-    """Child of list base class"""
+    ''' Child of list base class '''
     
     def print_sorted(self):
         """ Print mylist in ascending order """
+        
         sorted_list = sorted(self)
         print(sorted_list)
