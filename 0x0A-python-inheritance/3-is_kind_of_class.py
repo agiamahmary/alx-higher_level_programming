7ba799d1ca6c1f8a7774ebdfd34dc937e7c4f2b9
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-01 18:58:15 +0100

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ Contains function `is_kind_of_class`
-
--that returns true if 
+-that returns true if
 object is the exact or `indirect` instance
-of the specified class 
+of the specified class
 """
 def is_kind_of_class(obj, a_class):
     """ Return true if obj is a direct or indirect instance of a_class """
