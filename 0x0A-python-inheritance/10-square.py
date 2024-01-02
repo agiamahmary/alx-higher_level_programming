249b2fcf5c8ff1bf78500508a7062f0be1aa5458
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-02 23:57:16 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
-"""Contains a Square class """
+""" Contains a Square class """
+Rectangle = __import__('8-rectangle').Rectangle
 
 
 class Square(Rectangle):
