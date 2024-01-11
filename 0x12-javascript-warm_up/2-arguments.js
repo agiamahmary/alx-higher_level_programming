419d9d4b798965a534772cc4d6def0f359fac188
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-11 01:23:23 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,4 +4,4 @@
 const args = process.argv;
 
 if (args.length <= 2) console.log('No argument');
-else console.log('Argument found');
+else console.log((args.length === 3) ? 'Argument found' : 'Arguments found');
