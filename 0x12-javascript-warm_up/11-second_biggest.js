5877958987f7368c54ca90d0a45f0f92c58ab78d
maryagiamh <maryagiamh@gmail.com>
2024-01-10 17:26:56 -0800

    Fixed Syntax

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,8 +5,8 @@ const args = process.argv;
 
 if (args.length === 2) console.log(0);
 let num1 = 1;
-let max  = Math.floor(Number(args[2]));
-for (let x in args.splice(2)) {
+let max = Math.floor(Number(args[2]));
+for (const x in args.splice(2)) {
   num1 = Math.floor(Number(x));
   if (num1 > max) max = num1;
 }
