dc4396400dc9c8e56f900ccf02523c389992c0c4
Under20techie <under20techie@gmail.com>
2024-08-27 18:43:57 +0100

    you

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  function myConverter (number) {
-    let ans = '';
-    const digit = '0123456789abcdef';
+  let ans = '';
+  const digit = '0123456789abcdef';
 
+  function myConverter (number) {
     while (number > 0) {
       ans += (digit[number % base]).toString();
       number = Math.floor(number / base);
