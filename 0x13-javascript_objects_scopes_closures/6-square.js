9f969267017d028337cdefd86dc038cdbdcad5f9
Under20techie <under20techie@gmail.com>
2024-08-27 14:43:53 +0100

    you

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Parent = require('./5-square');
+
+class Square extends Parent {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint (c) {
+    if (!c) {
+      c = 'X'
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c);
+      }
+      console.log();
+    }
+  }
+}
+module.exports = Square;
