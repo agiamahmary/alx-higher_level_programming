d8cd82e2e701fee45116e895ad0e697de0fbfc41
Under20techie <under20techie@gmail.com>
2024-08-27 12:59:07 +0100

    you

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function incr() {
-  return this.value += 1;
-}
+myObject.incr = function incr () {
+  this.value += 1;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
