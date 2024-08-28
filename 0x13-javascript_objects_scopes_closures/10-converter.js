b8674d6846471f99ef0bddc8bfdcd3cc99828296
Under20techie <under20techie@gmail.com>
2024-08-28 12:02:12 +0100

    you

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   function myConverter (number) {
-    if (number >= 0) {
-      if ((number % base) > 10) {
-        return String.fromCharCode('A'.charCodeAt(0) + (number % base) - 10);
+    if (number < base) {
+      if (number <= 10) {
+        return number.toString();
       }
-      return myConverter(Math.floor(number / base)) + (number % base).toString();
+      return String.fromCharCode('A'.charCodeAt(0) + (number % base) - 10);
     }
-    return '';
+    if ((number % base) > 10) {
+      return myConverter(Math.floor(number / base)) + String.fromCharCode('A'.charCodeAt(0) + (number % base) - 10);
+    }
+    return myConverter(Math.floor(number / base)) + (number % base).toString();
   }
   return myConverter;
 };
