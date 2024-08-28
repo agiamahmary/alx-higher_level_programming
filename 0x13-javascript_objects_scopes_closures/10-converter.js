f610d1739222af1394236e680c782f4105ba9c3a
Under20techie <under20techie@gmail.com>
2024-08-28 11:32:34 +0100

    you

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   function myConverter (number) {
-    if (number > 0) {
+    if (number >= 0) {
       if ((number % base) > 10) {
         return String.fromCharCode('A'.charCodeAt(0) + (number % base) - 10);
       }
