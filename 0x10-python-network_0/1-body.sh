3f3940b2915490c0aa323b6c4ff034ee016ddcc1
Under20techie <under20techie@gmail.com>
2024-09-04 15:03:54 +0100

    you

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that displays the body of the response
-curl -sL -w " %{http_code}"  "$1" | sed -n 's/200//p'
+curl -sL -w "%{http_code}"  "$1" | sed -n 's/200//p'
