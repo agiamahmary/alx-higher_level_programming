58cf570854c44d2476d528300e2c88b44c500c01
Under20techie <under20techie@gmail.com>
2024-09-06 16:06:58 +0100

    you

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL and displays the body of the response
-curl -d @"$2" -H "Content-Type: application/json" "$1"
+curl -sL -d @"$2" -H "Content-Type: application/json" "$1"
