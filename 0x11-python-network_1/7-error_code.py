4ecf5ef0f6abd4121968f98e9d52714813a6eb4d
Under20techie <under20techie@gmail.com>
2024-09-07 13:26:30 +0100

    you'

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL,
+    sends a request to the URL and displays
+    the body of the response.
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    try:
+        r = requests.get(url)
+        if r.status_code >= 400:
+            r.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print("Error code: {}".format(e.response.status_code))
