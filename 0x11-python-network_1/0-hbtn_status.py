90eb6d40a919ee04b09365d2cabd56118039eed3
Under20techie <under20techie@gmail.com>
2024-09-07 11:14:28 +0100

    you

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Write a Python script that fetches
+    https://alx-intranet.hbtn.io/status
+"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as res:
+
+        byte_v = res.read()
+        typ = type(byte_v)
+        utf_v = byte_v.decode('utf-8')
+
+        print("""Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {}""".format(typ, byte_v, utf_v))
